9c2e851b7117a5332c8500ad704660f91cbb93f1
Alex Frank <alexfrankcodes@gmail.com>
2021-02-27 10:07:39 -0800

    Started 'Uses' page

---
+++ pages/uses.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+const Uses = () => {
+  return (
+    <div>
+      <h1>Uses</h1>
+      <h2>Languages</h2>
+      <ul>
+        <li>TypeScript</li>
+        <li>JavaScript</li>
+        <li>Python</li>
+        <li>C++</li>
+      </ul>
+      <h2>Editor</h2>
+      <ul>
+        <li>Visual Studio Code</li>
+      </ul>
+    </div>
+  );
+};
+
+export default Uses;
