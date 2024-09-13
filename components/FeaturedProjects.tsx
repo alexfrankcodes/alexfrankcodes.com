b0b9a8eec96d1e0f462f421f244441e8aea1847c
Alex Frank <alexfrankcodes@gmail.com>
2024-09-13 10:49:10 -0700

    feat: Adding basic animations

--- components/FeaturedProjects.tsx
+++ components/FeaturedProjects.tsx
@@ -1,3 +1,6 @@
+"use client";
+
+import { motion } from "framer-motion";
 import Link from "next/link";
 import { FaGithub } from "react-icons/fa";
 
@@ -16,7 +19,10 @@ const ProjectCard = ({
   link,
   github,
 }: ProjectCardProps) => (
-  <div className="bg-gray-800 rounded-lg p-6 flex flex-col justify-between">
+  <motion.div
+    className="bg-gray-800 rounded-lg p-6 flex flex-col justify-between"
+    whileHover={{ scale: 1.05, y: 1, x: -1 }}
+  >
     <div className="flex flex-col justify-between">
       <h3 className="text-2xl font-bold mb-2 text-white">{title}</h3>
       <p className="text-gray-400 mb-4">{description}</p>
@@ -67,7 +73,7 @@ const ProjectCard = ({
         </Link>
       )}
     </div>
-  </div>
+  </motion.div>
 );
 
 const FeaturedProjects = () => {
