bd56d691a6fcd1d4d08b993b87cede1d8cf3c02d
Alexander <alexfrankcodes@gmail.com>
2024-10-05 11:09:33 -0700

    Skeleton Components for page load

--- app/page.tsx
+++ app/page.tsx
@@ -2,29 +2,48 @@ import dynamic from "next/dynamic";
 
 import Navbar from "../components/common/Navbar";
 import ProgressBar from "../components/common/ProgressBar";
+import {
+  LandingSkeleton,
+  AboutSkeleton,
+  ProjectsSkeleton,
+  MentorshipSkeleton,
+  ContactSkeleton,
+  FooterSkeleton
+} from "../components/common/SkeletonLoader";
 
 const About = dynamic(() => import("../components/about/About"), {
-  loading: () => <p>Loading...</p>,
+  loading: () => <AboutSkeleton />,
+  ssr: false,
 });
+
 const Contact = dynamic(() => import("../components/Contact"), {
-  loading: () => <p>Loading...</p>,
+  loading: () => <ContactSkeleton />,
+  ssr: false,
 });
+
 const FeaturedProjects = dynamic(
   () => import("../components/projects/FeaturedProjects"),
   {
-    loading: () => <p>Loading...</p>,
+    loading: () => <ProjectsSkeleton />,
+    ssr: false,
   }
 );
+
 const Footer = dynamic(() => import("../components/common/Footer"), {
-  loading: () => <p>Loading...</p>,
+  loading: () => <FooterSkeleton />,
+  ssr: false,
 });
+
 const Landing = dynamic(() => import("../components/Landing"), {
-  loading: () => <p>Loading...</p>,
+  loading: () => <LandingSkeleton />,
+  ssr: false,
 });
+
 const Mentorship = dynamic(
   () => import("../components/mentorship/Mentorship"),
   {
-    loading: () => <p>Loading...</p>,
+    loading: () => <MentorshipSkeleton />,
+    ssr: false,
   }
 );
 
