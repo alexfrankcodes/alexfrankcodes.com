8425c0c993a87358a4dfa3f8a071bf854b768de6
Alexander <alexfrankcodes@gmail.com>
2025-05-22 16:04:20 -0700

    Remove skeleton loader for faster load (#10)

--- app/page.tsx
+++ app/page.tsx
@@ -1,51 +1,11 @@
-import dynamic from "next/dynamic";
-
 import Navbar from "../components/common/Navbar";
 import ProgressBar from "../components/common/ProgressBar";
-import {
-  LandingSkeleton,
-  AboutSkeleton,
-  ProjectsSkeleton,
-  MentorshipSkeleton,
-  ContactSkeleton,
-  FooterSkeleton
-} from "../components/common/SkeletonLoader";
-
-const About = dynamic(() => import("../components/about/About"), {
-  loading: () => <AboutSkeleton />,
-  ssr: false,
-});
-
-const Contact = dynamic(() => import("../components/Contact"), {
-  loading: () => <ContactSkeleton />,
-  ssr: false,
-});
-
-const FeaturedProjects = dynamic(
-  () => import("../components/projects/FeaturedProjects"),
-  {
-    loading: () => <ProjectsSkeleton />,
-    ssr: false,
-  }
-);
-
-const Footer = dynamic(() => import("../components/common/Footer"), {
-  loading: () => <FooterSkeleton />,
-  ssr: false,
-});
-
-const Landing = dynamic(() => import("../components/Landing"), {
-  loading: () => <LandingSkeleton />,
-  ssr: false,
-});
-
-const Mentorship = dynamic(
-  () => import("../components/mentorship/Mentorship"),
-  {
-    loading: () => <MentorshipSkeleton />,
-    ssr: false,
-  }
-);
+import About from "../components/about/About";
+import Contact from "../components/Contact";
+import FeaturedProjects from "../components/projects/FeaturedProjects";
+import Footer from "../components/common/Footer";
+import Landing from "../components/Landing";
+import Mentorship from "../components/mentorship/Mentorship";
 
 export default function Home() {
   return (
